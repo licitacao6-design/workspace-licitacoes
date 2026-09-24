bec72efdb2f82cd82d21095d0edaa0b27870bc8e
licitacao6-design <licitacao6@gmail.com>
2026-09-24 15:37:11 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -127,5 +127,5 @@ def excluir_editais():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    print("Servidor iniciado em http://localhost:5000")
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
